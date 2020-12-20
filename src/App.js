dda8674bd4a0c237347308eeaab17f9829707ae2
livm <livm@student.chalmers.se>
2020-12-20 22:57:05 +0100

    Main app sections rendered on App mount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { AppFooter } from './components'
+import {
+  HeroSection,
+  LandingSection,
+  PortfolioSection,
+} from './sections'
+
+import './App.css'
+
+const sectionClasses =
+  ' container mx-auto px-5 sm:px-6 md:px-14 lg:px-20'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <HeroSection />
+      <LandingSection classes={sectionClasses} />
+      <PortfolioSection classes={sectionClasses} />
+      <AppFooter classes={sectionClasses} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
